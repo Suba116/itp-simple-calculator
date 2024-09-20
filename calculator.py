926c03864f988bea91c9ebdaeb0fb6d39671d2f7
Suba <subaanandcdm@gmail.com>
2024-09-20 21:44:28 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -3,24 +3,45 @@ def add(x, y):
 
 
 def subtract(x, y):
-    pass
+    return x - y
 
 
 def divide(x, y):
-    pass
+    if y == 0 :
+        return invalid
+    else :
+        return x/y
 
 
 def multiply(x, y):
-    pass
+    return x * y 
 
 
 def square(x):
-    pass
+    return x ** 2
 
 
 def power(x, y):
-    pass
+    return x ** y
 
 
 def sqrt(x):
-    pass
+    return x ** 0.5
+
+def calculator (choice, x , y ) :
+    if choice == '1' : 
+        return add(x, y)
+    elif choice == '2':
+        return subtract(x, y)
+    elif choice == '3':
+        return divide(x,y)
+    elif choice == '4':
+        return multiply(x, y)
+    elif choice == '5':
+        return square(x)
+    elif choice == '6':
+        return power(x, y)
+    elif choice == '7':
+        return sqrt(x)
+    else:
+        return error
